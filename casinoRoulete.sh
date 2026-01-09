f2e0e4719a70743f2eab5031fbd00649591a215a
 Marco Figueroa <marcofigueroa23@icloud.com>
2026-01-09 16:40:22 -0300

    feature: adding martingala technique

--- casinoRoulete.sh
+++ casinoRoulete.sh
@@ -13,7 +13,7 @@ grayColour="\e[0;37m\033[1m"
 # Funcion para salir del programa (CTRL + C)
 function ctrl_c () {
   echo -e "\n${redColour}[!]${endColour} ${yellowColour}Saliendo...${endColour}\n"
-  exit 1
+  tput cnorm && exit 1
 }
 
 # Capturando CTRL + C
@@ -28,12 +28,69 @@ function helpPanel () {
 
 # Funcion tecnica martingala
 funcion martingala () {
-  echo -e "\n${greenColour}[+]${endColour} Utilizando tecnica ${yellowColour}Martingala${endColour}\n"
+  echo -e "\n${greenColour}[+]${endColour} ${grayColour}Utilizando tecnica${endColour} ${yellowColour}Martingala${endColour} ${grayColour}en la ruleta${endColour}\n"
+  echo -ne "${greenColour}[?]${endColour} ${grayColour}¿Cuanto${endColour} ${yellowColour}dinero${endColour} ${grayColour}desea apostar?:${endColour} ${greenColour}\$${endColour}" && read bet
+  while [[ ! "$bet" =~ ^[0-9]+$ ]]; do
+    echo -ne "${greenColour}[?]${endColour} ${grayColour}¿Cuanto${endColour} ${yellowColour}dinero${endColour} ${grayColour}desea apostar?:${endColour} ${greenColour}\$${endColour}" && read bet
+  done
+  echo -ne "${greenColour}[?]${endColour} ${grayColour}¿A que desea apostar${endColour} ${yellowColour}continuamente${endColour}${grayColour}?${endColour} ${greenColour}[par/impar]${endColour}${grayColour}:${endColour} " && read place_bet
+  while [ "$place_bet" != "par" ] && [ "$place_bet" != "impar" ]; do 
+    echo -ne "${greenColour}[?]${endColour} ${grayColour}¿A que desea apostar${endColour} ${yellowColour}continuamente${endColour}${grayColour}?${endColour} ${greenColour}[par/impar]${endColour}${grayColour}:${endColour} " && read place_bet
+  done
+  echo -e "\n${blueColour}[i]${endColour} ${grayColour}Realizando apuesta de ${greenColour}\$$bet${endColour} ${grayColour}continuamente a los numeros${endColour} ${yellowColour}$place_bet${endColour}\n"
+  tput civis
+  bet_backup=$bet
+  while true; do
+    if [ "$money" -eq 0 ]; then
+      echo -e "${redColour}[!] Te has quedado sin dinero \$$money${endColour}\n"
+      tput cnorm && exit 1
+    elif [ "$bet" -gt "$money" ]; then
+      echo -e "${redColour}[!] No hay dinero para realizar la apuesta"
+      echo -e "${redColour}[!]${endColour} ${grayColour}Dinero actual:${endColour} ${greenColour}\$$money${endColour}\n"
+      tput cnorm && exit 1
+    fi
+    money=$(($money-$bet))
+    random_number="$(($RANDOM % 37))"
+    echo -e "${greenColour}[+]${endColour} ${grayColour}Apostando:${endColour} ${yellowColour}\$$bet${endColour} ${grayColour}| Dinero Restante:${endColour} ${greenColour}\$$money${endColour}"
+    echo -e "${greenColour}[+]${endColour} ${grayColour}Ha salido el numero:${endColour} ${yellowColour}$random_number${endColour}"
+    if [ "$(($random_number % 2))" -eq 0 ]; then
+      if [ "$random_number" -eq 0 ]; then
+        echo -e "\t${blueColour}[i]${endColour} ${grayColour}El numero es${endColour} ${yellowColour}0${endColour}${grayColour}. Hemos perdido${endColour}"
+      fi
+      echo -e "\t${blueColour}[i]${endColour} ${grayColour}El numero es${endColour} ${yellowColour}par${endColour}"
+      if [ "$place_bet" = "par" ]; then
+        reward=$(($bet*2))
+        money=$(($money+$reward))
+        echo -e "\t${greenColour}[+] ¡Has Ganado \$$reward!${endColour}"
+        echo -e "\t${blueColour}[i]${endColour} ${grayColour}Dinero Actual:${endColour} ${greenColour}\$$money${endColour}\n"
+        bet=$bet_backup
+      else
+        bet=$(($bet*2))
+        echo -e "\t${redColour}[!] ¡Has Perdido!${redColour}"
+        echo -e "\t${redColour}[!]${endColour} ${grayColour}Apostando el doble${endColour} ${yellowColour}\$$bet${endColour}\n"
+      fi
+    else
+      echo -e "\t${blueColour}[i]${endColour} ${grayColour}El numero es${endColour} ${yellowColour}impar${endColour}"
+      if [ "$place_bet" = "impar" ]; then
+        reward=$(($bet*2))
+        money=$(($money+$reward))
+        echo -e "\t${greenColour}[+] ¡Has Ganado \$$reward!${endColour}"
+        echo -e "\t${blueColour}[i]${endColour} ${grayColour}Dinero Actual:${endColour} ${greenColour}\$$money${endColour}\n"
+        bet=$bet_backup
+      else
+        bet=$(($bet*2))
+        echo -e "\t${redColour}[!] ¡Has Perdido!${redColour}"
+        echo -e "\t${redColour}[!]${endColour} ${grayColour}Apostando el doble${endColour} ${yellowColour}\$$bet${endColour}\n"
+      fi
+    fi
+    sleep 3
+  done
+  tput cnorm
 }
 
 # Funcion tecnica labouchere inversa
 function labouchereInversa () {
-  echo -e "\n${greenColour}[+]${endColour} Utilizando tecnica ${yellowColour}Labouchere Inversa${endColour}\n"
+  echo -e "\n${greenColour}[+]${endColour} ${grayColour}Utilizando tecnica${endColour} ${yellowColour}Labouchere Inversa${endColour} ${grayColour}en la ruleta${endColour}\n"
 }
 
 # Definiendo opciones validas del programa
