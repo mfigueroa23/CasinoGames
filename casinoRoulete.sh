4cdff094108953bb9a2f57745d80b6d99532bb81
 Marco Figueroa <marcofigueroa23@icloud.com>
2026-01-11 16:11:36 -0300

    feature: adding case win

--- casinoRoulete.sh
+++ casinoRoulete.sh
@@ -23,7 +23,7 @@ trap ctrl_c INT
 function helpPanel () {
   echo -e "\n${greenColour}[i]${endColour} ${grayColour}Para el uso correcto de la herramienta se debe proporcionar los siguientes parametros:${endColour}\n"
   echo -e "\t${blueColour}[+]${endColour} ${greenColour}-m${endColour} ${yellowColour}<numero>${endColour}\t ${grayColour}Dinero a utilizar (ficticio)${endColour}"
-  echo -e "\t${blueColour}[+]${endColour} ${greenColour}-t${endColour} ${yellowColour}<nombre>${endColour}\t ${grayColour}Tecnica a utilizar [ martingala, labouchereInversa ]${endColour}\n"
+  echo -e "\t${blueColour}[+]${endColour} ${greenColour}-t${endColour} ${yellowColour}<nombre>${endColour}\t ${grayColour}Tecnica a utilizar${endColour} [ ${yellowColour}martingala${endColour}${grayColour},${endColour} ${yellowColour}labouchereInversa${endColour} ${grayColour}]${endColour}\n"
 }
 
 # Funcion tecnica martingala
@@ -125,6 +125,97 @@ funcion martingala () {
 # Funcion tecnica labouchere inversa
 function labouchereInversa () {
   echo -e "\n${greenColour}[+]${endColour} ${grayColour}Utilizando tecnica${endColour} ${yellowColour}Labouchere Inversa${endColour} ${grayColour}en la ruleta${endColour}\n"
+  echo -ne "${greenColour}[?]${endColour} ${grayColour}¿A que desea apostar${endColour} ${yellowColour}continuamente${endColour}${grayColour}?${endColour} ${greenColour}[par/impar]${endColour}${grayColour}:${endColour} " && read place_bet
+  while [ "$place_bet" != "par" ] && [ "$place_bet" != "impar" ]; do 
+    echo -ne "${greenColour}[?]${endColour} ${grayColour}¿A que desea apostar${endColour} ${yellowColour}continuamente${endColour}${grayColour}?${endColour} ${greenColour}[par/impar]${endColour}${grayColour}:${endColour} " && read place_bet
+  done
+  declare -a my_sequence=(1 2 3 4)
+  tput civis
+  reward=0
+  total_plays=0
+  max_money_ammount=0
+  max_money_ammount_play=0
+  total_win=0
+  total_lose=0
+  bad_results=""
+  while true; do
+    if [ "$money" -eq 0 ]; then
+      echo -e "${redColour}[!] Te has quedado sin dinero \$$money${endColour}\n"
+      echo -e "${blueColour}[i]${endColour} ${grayColour}Ha habido un total de${endColour} ${yellowColour}$total_plays${yellowColour} ${grayColour}jugadas${endColour}"
+      echo -e "${blueColour}[i]${endColour} ${grayColour}Ha llegado a tener un maximo de${endColour} ${greenColour}\$$max_money_ammount${endColour} ${grayColour}en la jugada${endColour} ${yellowColour}$max_money_ammount_play${endColour}"
+      echo -e "${blueColour}[i]${endColour} ${grayColour}Ha llegado a ganar un total de${endColour} ${greenColour}$total_win${endColour} ${grayColour}veces${endColour}"
+      echo -e "${blueColour}[i]${endColour} ${grayColour}Ha llegado a perder un total de${endColour} ${redColour}$total_lose${endColour} ${grayColour}veces${endColour}"
+      echo -e "${blueColour}[i]${endColour} ${grayColour}Los resultados malos obtenidos son:${endColourColour} ${yellowColour}$bad_results${yellowColour}\n"
+      tput cnorm && exit 1
+    elif [ "$bet" -gt "$money" ]; then
+      echo -e "${redColour}[!] No hay dinero para realizar la apuesta"
+      echo -e "${redColour}[!]${endColour} ${grayColour}Dinero actual:${endColour} ${greenColour}\$$money${endColour}\n"
+      echo -e "${blueColour}[i]${endColour} ${grayColour}Ha habido un total de${endColour} ${yellowColour}$total_plays${yellowColour} ${grayColour}jugadas${endColour}"
+      echo -e "${blueColour}[i]${endColour} ${grayColour}Ha llegado a tener un maximo de${endColour} ${greenColour}\$$max_money_ammount${endColour} ${grayColour}en la jugada${endColour} ${yellowColour}$max_money_ammount_play${endColour}"
+      echo -e "${blueColour}[i]${endColour} ${grayColour}Ha llegado a ganar un total de${endColour} ${greenColour}$total_win${endColour} ${grayColour}veces${endColour}"
+      echo -e "${blueColour}[i]${endColour} ${grayColour}Ha llegado a perder un total de${endColour} ${redColour}$total_lose${endColour} ${grayColour}veces${endColour}"
+      echo -e "${blueColour}[i]${endColour} ${grayColour}Los resultados malos obtenidos son:${endColourColour} ${yellowColour}$bad_results${yellowColour}\n"
+      tput cnorm && exit 1
+    fi
+    echo -e "\n${blueColour}[i]${endColour} ${grayColour}Nuestra secuencia es${endColour} ${greenColour}[ ${my_sequence[@]} ]${endColour}"
+    if [ "${#my_sequence[@]}" -ne 1 ]; then
+      bet=$((${my_sequence[1]} + ${my_sequence[-1]}))
+    elif [ "${#my_sequence[@]}" -eq 1 ]; then
+      bet=${my_sequence[1]}
+    fi
+    money=$(($money - $bet))
+    random_number=$(($RANDOM % 37))
+    let total_plays+=1
+    echo -e "${blueColour}[i]${endColour} ${grayColour}Realizando apuesta de ${greenColour}\$$bet${endColour} ${grayColour}continuamente a los numeros${endColour} ${yellowColour}$place_bet${endColour}\n"
+    echo -e "${greenColour}[+]${endColour} ${grayColour}Apostando:${endColour} ${yellowColour}\$$bet${endColour} ${grayColour}| Dinero Restante:${endColour} ${greenColour}\$$money${endColour}"
+    echo -e "${greenColour}[+]${endColour} ${grayColour}Ha salido el numero:${endColour} ${yellowColour}$random_number${endColour}"
+    if [ "$(($random_number % 2))" -eq 0 ]; then
+      if [ "$random_number" -eq 0 ]; then
+        echo -e "\t${blueColour}[i]${endColour} ${grayColour}El numero es${endColour} ${yellowColour}0${endColour}${grayColour}. Hemos perdido${endColour}"
+        bad_results="$random_number "
+        let total_lose+=1
+      fi
+      echo -e "\t${blueColour}[i]${endColour} ${grayColour}El numero es${endColour} ${yellowColour}par${endColour}"
+      if [ "$place_bet" = "par" ]; then
+        reward=$(($bet*2))
+        money=$(($money + $bet))
+        let total_win+=1
+        if [ $money -ge $max_money_ammount ]; then
+          max_money_ammount=$money
+          max_money_ammount_play=$total_plays
+        fi
+        my_sequence+=($bet)
+        my_sequence=(${my_sequence[@]})
+        echo -e "\t${greenColour}[+] ¡Has Ganado \$$reward!${endColour}"
+        echo -e "\t${blueColour}[i]${endColour} ${grayColour}Dinero Actual:${endColour} ${greenColour}\$$money${endColour}\n"
+      else
+        bad_results="$random_number "
+        let total_lose+=1
+        echo -e "\t${redColour}[!] ¡Has Perdido!${redColour}"
+      fi
+    else
+      echo -e "\t${blueColour}[i]${endColour} ${grayColour}El numero es${endColour} ${yellowColour}impar${endColour}"
+      if [ "$place_bet" = "impar" ]; then
+        reward=$(($bet*2))
+        money=$(($money + $bet))
+        let total_win+=1
+        if [ $money -ge $max_money_ammount ]; then
+          max_money_ammount=$money
+          max_money_ammount_play=$total_plays
+        fi
+        my_sequence+=($bet)
+        my_sequence=(${my_sequence[@]})
+        echo -e "\t${greenColour}[+] ¡Has Ganado \$$reward!${endColour}"
+        echo -e "\t${blueColour}[i]${endColour} ${grayColour}Dinero Actual:${endColour} ${greenColour}\$$money${endColour}\n"
+      else
+        bad_results="$random_number "
+        let total_lose+=1
+        echo -e "\t${redColour}[!] ¡Has Perdido!${redColour}"
+      fi
+    fi
+    sleep 0.25
+  done
+  tput cnorm
 }
 
 # Definiendo opciones validas del programa
